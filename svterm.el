8748d1dd76b3a46305250573bcc09f47e1830316
Daniel Tschertkow <daniel.tschertkow@posteo.de>
2024-12-21 13:09:31 +0100

    Add docstrings

--- svterm.el
+++ svterm.el
@@ -9,26 +9,32 @@
   "vector of lengthvterm buffers")
 
 (defun svterm-advance-index! ()
+  "Return next index of `svterm-buffer-index'."
   (setq svterm-buffer-index
         (mod (+ svterm-buffer-index 1) svterm-buffer-count)))
 
 (defun svterm-current ()
+  "Return current buffer of `svterm-buffers' pointed to by `svterm-buffer-index'."
   (aref svterm-buffers svterm-buffer-index))
 
 (defun svterm-next-slot! ()
+  "Return the buffer of `svterm-buffers' in the slot next in line or nil, if it
+is free."
   (svterm-advance-index!)
   (if (buffer-live-p (svterm-current))
       (svterm-current)
     (aset svterm-buffers svterm-buffer-index nil)))
 
-(defun svterm-find-next ()
+(defun svterm-find-next! ()
+  "Iterate through `svterm-buffers' and return the next live `vterm' buffer."
   (let ((start-i vterm-buffer-index))
     (while (not (= vterm-buffer-index start-i))
       (when (svterm-next-slot!)
         (return (svterm-current)))
       )))
 
-(defun svterm-find-free ()
+(defun svterm-find-free! ()
+  "Iterate through `svterm-buffers' and return the index of the next free slot."
   (let ((start-i vterm-buffer-index))
     (while (not (= vterm-buffer-index start-i))
       (unless (svterm-next-slot!)
@@ -36,6 +42,7 @@
       )))
 
 (defun svterm-make-buffer ()
+  "Create and return a new `vterm' buffer."
   (let ((buf (generate-new-buffer (format "*vterm*<%d>" svterm-buffer-index))))
     (with-current-buffer buf
       (vterm-mode))
@@ -49,6 +56,8 @@
 
       )
     )
+  (if (svterm-find-free)
+      ())
   )
 
 
